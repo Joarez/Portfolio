581dc5cfcfad84f56d6b4c790cd36e69506c2c95
Joarez Rosa <13007827+Joarez@users.noreply.github.com>
2026-07-20 11:37:27 -0300

    Implement page flipping feature with buttons
    Added functionality for page flipping with buttons.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-const reveals = document.querySelectorAll(".reveal");
-window.addEventListener("scroll", () => {
-    reveals.forEach((element) => {
-        const windowHeight = window.innerHeight;
-        const revealTop = element.getBoundingClientRect().top;
-        if(revealTop < windowHeight - 100){
-            element.classList.add("active");
+const pages = document.querySelectorAll(".page");
+let currentPage = 0;
+
+/* CAPA */
+const openButton = document.querySelector(".open-book-btn");
+if(openButton){
+    openButton.addEventListener("click", () => {
+        if(currentPage < pages.length - 1){
+            pages[currentPage].classList.add("flipped");
+            currentPage++;
         }
     });
-    
+}
+
+/* PRÓXIMAS PÁGINAS */
 const nextButtons = document.querySelectorAll(".next-page-btn");
 nextButtons.forEach(button => {
     button.addEventListener("click", () => {
@@ -16,4 +21,4 @@ nextButtons.forEach(button => {
             currentPage++;
         }
     });
-});
+});;
