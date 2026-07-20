bb9b401f5509ebdb9a83f81b7501691822e832f3
Joarez Rosa <13007827+Joarez@users.noreply.github.com>
2026-07-20 11:23:01 -0300

    Refactor page navigation to use next buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const pages = document.querySelectorAll(".page");
-let currentPage = 0;
-document.addEventListener("click", () => {
-    if(currentPage < pages.length - 1){
-        pages[currentPage].classList.add("flipped");
-        currentPage++;
-    }
+const nextButtons = document.querySelectorAll(".next-page-btn");
+nextButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(currentPage < pages.length - 1){
+            pages[currentPage].classList.add("flipped");
+            currentPage++;
+        }
+    });
 });
